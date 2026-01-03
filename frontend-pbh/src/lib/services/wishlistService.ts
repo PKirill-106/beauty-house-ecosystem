dbf23ed9fbffa4216ce3d2ef9f98da3e03fdade0
Kyryl <p.kirill.design@gmail.com>
2026-01-03 14:45:31 +0100

    feat: implement wishist services

---
+++ frontend-pbh/src/lib/services/wishlistService.ts
@@ -0,0 +1,43 @@
+'use server'
+
+import { api } from '../api/axios'
+
+export async function getAllProductsFromWishList() {
+	const { data } = await api
+		.get('/WhisList/GetAllProductsFromWishList')
+		.catch(error => {
+			throw new Error('Failed to fetch wishlist: ', error)
+		})
+
+	return data.data
+}
+
+export async function addProductToWishList(productId: string) {
+	const { data } = await api
+		.post(`/WhisList/AddProductToWishList?productId=${productId}`)
+		.catch(error => {
+			throw new Error('Failed to add to wishlist: ', error)
+		})
+
+	return data.data
+}
+
+export async function updateWishList(wishlistItemId: string[]) {
+	const { data } = await api
+		.put('/WhisList/UpdateWishList', JSON.stringify(wishlistItemId))
+		.catch(error => {
+			throw new Error('Failed to update wishlist: ', error)
+		})
+
+	return data.data
+}
+
+export async function removeProductFromWishList(wishlistItemId: string) {
+	const { data } = await api
+		.delete(`/WhisList/RemoveWishList?whishListItemId=${wishlistItemId}`)
+		.catch(error => {
+			throw new Error('Failed to remove product from wishlist: ', error)
+		})
+
+	return data.data
+}
