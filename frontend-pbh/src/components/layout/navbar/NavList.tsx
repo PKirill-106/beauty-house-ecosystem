70da1af8357a71f362e2bab2519c5f6af3cdfa49
Kyryl <p.kirill.design@gmail.com>
2026-01-25 11:34:05 +0100

    style: change gap in NavList component

--- frontend-pbh/src/components/layout/navbar/NavList.tsx
+++ frontend-pbh/src/components/layout/navbar/NavList.tsx
@@ -3,7 +3,7 @@ import { navlinkData } from './navlink-data'
 
 export default function NavList() {
 	return (
-		<ul className='hidden md:flex items-center justify-end gap-7 lg:gap-8'>
+		<ul className='hidden md:flex items-center justify-end gap-5 lg:gap-6'>
 			{navlinkData.map(el => (
 				<li key={el.tip}>
 					<MyTooltip element={el.icon} tip={el.tip} />
