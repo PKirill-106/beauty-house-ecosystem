8619f0b6f3d3d767646f7dea38bd01c8e709f210
Kyryl <p.kirill.design@gmail.com>
2026-01-27 15:15:01 +0100

    feat: implement getCategoryPath function

--- frontend-pbh/src/lib/utils/helpers.ts
+++ frontend-pbh/src/lib/utils/helpers.ts
@@ -9,11 +9,31 @@ export const getProfileLink = ({ role, status }: IGetProfileLink) => {
 
 	return profileHref
 }
-export const getSubcategories = (categories: ICategory[], parentId: string) => {
+export const getSubcategories = (
+	categories: ICategory[] | undefined,
+	parentId: string,
+) => {
+	if (!categories) return []
 	const subcategories = categories.filter(
 		cat => cat.parentCategoryId === parentId,
 	)
 	return subcategories
+}
+export const getCategoryPath = (
+	current: ICategory,
+	allCategories: ICategory[],
+) => {
+	const path: string[] = []
+
+	let category: ICategory | undefined = current
+
+	while (category) {
+		path.push(category.slug)
+		if (!category.parentCategoryId) break
+
+		category = allCategories.find(c => c.id === category?.parentCategoryId)
+	}
+	return path.reverse().join('/')
 }
 export const triggerAnimation = ({ setCount }: ITriggerAnimation) => {
 	setCount(prev => prev + 1)
