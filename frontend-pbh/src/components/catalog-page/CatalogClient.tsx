691a33ed15631be519dc029716104b5657ce4905
Kyryl <p.kirill.design@gmail.com>
2026-03-10 17:43:53 +0100

    fix: remove console log

--- frontend-pbh/src/components/catalog-page/CatalogClient.tsx
+++ frontend-pbh/src/components/catalog-page/CatalogClient.tsx
@@ -5,7 +5,6 @@ import ProductGrid from './ProductGrid'
 
 export default function CatalogClient() {
 	const { categories, paginatedProducts, activeCategory } = useFilters()
-console.log(categories, paginatedProducts, activeCategory)
 	return (
 		<>
 			<h2 className='mb-6'>{activeCategory?.name || 'Каталог'}</h2>
